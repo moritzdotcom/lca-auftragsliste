f65c573e6f8feb284fe3af616f48af74615994b0
Moritz Löchner <momo.loechner@gmail.com>
2021-03-03 22:18:17 +0100

    Rails.ajax enabled in views

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    Rails: ['@rails/ujs']
+  })
+);
+
 module.exports = environment
