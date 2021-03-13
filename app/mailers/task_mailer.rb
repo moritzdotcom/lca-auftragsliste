991daea76932252202d9f68d5374aef055aa90d9
Moritz Löchner <momo.loechner@gmail.com>
2021-03-13 11:52:58 +0100

    ical attachment on mails if task has a due date

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -8,6 +8,18 @@ class TaskMailer < ApplicationMailer
     @partners = opts[:partners]
     partner_emails = opts[:partner_emails]
 
+    if @task.due_date
+      ical = Icalendar::Calendar.new
+      ical.event do |e|
+        e.dtstart     = Icalendar::Values::Date.new(@task.due_date.strftime('%Y%m%d'))
+        e.dtend       = Icalendar::Values::Date.new(@task.due_date.next_day.strftime('%Y%m%d'))
+        e.summary     = @task.title
+        e.description = @task.description
+        e.ip_class    = "PRIVATE"
+      end
+      attachments['event.ics'] = { :mime_type => 'text/calendar',    content: ical.to_ical }
+    end
+
     attachments["Auftrag Nr. #{@task.prefix_number}.pdf"] = File.read("public/tasks_pdf/#{@task.id}_#{@task.updated_at}.pdf")
     mail(to: partner_emails, from: @user.email, cc: @user.email, subject: "Auftrag Nr. #{@task.prefix_number}")
   end
